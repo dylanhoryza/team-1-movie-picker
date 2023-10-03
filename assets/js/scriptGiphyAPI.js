ba30fc1537c32469a722492f84e423d0a6783965
sjbdlt <141295016+sjbdlt@users.noreply.github.com>
2023-10-02 20:49:49 -0700

    Update Layout

--- assets/js/scriptGiphyAPI.js
+++ assets/js/scriptGiphyAPI.js
@@ -1,45 +1,45 @@
-//placing variable from index html. 
-var gifEl_1 = document.getElementById('giphy1') ;
-var btnSearch = document.getElementById('btnsearch');
+// //placing variable from index html. 
+// var gifEl_1 = document.getElementById('giphy1') ;
+// var btnSearch = document.getElementById('btnsearch');
 
-// placing my API Key here
-const giphyKey = "QRQ4VnDC8nQFb9LYpgi1mywDZ8oJ8C8i";
+// // placing my API Key here
+// const giphyKey = "QRQ4VnDC8nQFb9LYpgi1mywDZ8oJ8C8i";
 
 
-// placing the API fetch information here based off the keyword from the search bar. 
-function getGiphyData(){
-    const searchInput1 = document.getElementById('txtsearchval1').value;
-    console.log(typeof(searchInput1));
-    console.log(searchInput1); /// doesnt show me a txt just a blank ''
+// // placing the API fetch information here based off the keyword from the search bar. 
+// function getGiphyData(){
+//     const searchInput1 = document.getElementById('txtsearchval1').value;
+//     console.log(typeof(searchInput1));
+//     console.log(searchInput1); /// doesnt show me a txt just a blank ''
 
     
-    //const gifApiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}"&q=${searchInput1}&limit=1`;
+//     //const gifApiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}"&q=${searchInput1}&limit=1`;
 
-    const gifApiUrl = "https://api.giphy.com/v1/gifs/search?api_key=QRQ4VnDC8nQFb9LYpgi1mywDZ8oJ8C8i&q=superbad%27&limit=1"
+//     const gifApiUrl = "https://api.giphy.com/v1/gifs/search?api_key=QRQ4VnDC8nQFb9LYpgi1mywDZ8oJ8C8i&q=superbad%27&limit=1"
 
-    fetch(gifApiUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function (data) {
-        appendGifImage(data);
-    });
-};
-getGiphyData();
+//     fetch(gifApiUrl)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         appendGifImage(data);
+//     });
+// };
+// getGiphyData();
 
-// adding event for search button to pull the text from the search bar to append the gif image url
-btnsearch.addEventListener('click', getGiphyData)
+// // adding event for search button to pull the text from the search bar to append the gif image url
+// btnsearch.addEventListener('click', getGiphyData)
 
 
 
-function appendGifImage (data){
-    console.log(data);
-    const gifDisplay = data.data[0].images.original.url;
+// function appendGifImage (data){
+//     console.log(data);
+//     const gifDisplay = data.data[0].images.original.url;
     
-    console.log(gifDisplay);
-    document.querySelector('#giphy1').src = `${gifDisplay}`;
+//     console.log(gifDisplay);
+//     document.querySelector('#giphy1').src = `${gifDisplay}`;
     
-};
+// };
 
 // function onImgLoad(){
 //     return new Promise((resolve, reject) => {
